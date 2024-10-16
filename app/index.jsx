e71b90d09a7884fdb8e2789848bc7aab4868243f
Samisahariar <samisahariar69@gmail.com>
2024-10-16 09:31:01 -0700

    this is the first app

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>thats the first andorid or ios app i have ever built through</Text>
+    </View>
+  );
+}
